88f1f6aa456fa767dd463dffa8f630f9014c75b5
Omar Molina <A01423485@itesm.mx>
2021-03-22 19:45:01 -0600

    studio_1

---
+++ lab1/app/src/main/java/com/example/example1_2020/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.example1_2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+
+}
